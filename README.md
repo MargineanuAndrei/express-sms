ceb53d600bebab62c6d717fa84e1f83bff6ef47d
MargineanuAndrei <margineanuandrei94@gmail.com>
2018-01-03 14:46:14 +0200

    clear form after sending data & added description

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# express-sms
+# Express SMS App
+
+To run this app you need to have register to https://www.nexmo.com.
+
+And to create .env file , .env-example will help you .
+
+npm install
+
+npm start
--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,18 @@ button.addEventListener('click', send, false);
 const socket = io();
 socket.on('smsStatus', function(data) {
     response.innerHTML = '<h6>Text message sent to ' + data.number + '</h6>';
+    clearMsg();
 })
+
+function clearMsg() {
+    setTimeout(function() { response.innerHTML = ''; }, 3000);
+}
 
 function send() {
     const number = numberInput.value.replace(/\D/g, '');
     const text = textInput.value;
+    document.getElementById('number').value = '';
+    document.getElementById('msg').value = '';
 
     fetch('/', {
             method: 'post',
